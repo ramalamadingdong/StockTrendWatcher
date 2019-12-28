881a4d8cf97f7a5d2e57c92a9e898007b6a81bf8
rami <ramimouro@gmail.com>
2019-12-28 15:55:11 -0700

    stock data from yahoo

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ import feedparser
 import googlefinance
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
- 
+import bs4 as bs
+import urllib.request
+
+source = urllib.request.urlopen("https://finance.yahoo.com/gainers").read()
+soup = bs.BeautifulSoup(source, features="html.parser")
+table = soup.table
+table_rows = table.find_all('tr')
+rows = []
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    rows.append(row)
+rows.pop(0)
+gainers = pd.DataFrame(rows)
+gainers.columns = ['SYM', 'COMPANY', 'Current Price', 'Price Change', 'Percentage Change', 'Volume'	, 'Avg Vol', 'Market Cap', '52 Week Range', 'NaN']
+gainers['Price Change'] = gainers['Price Change'].str[1:]
+gainers['Price Change'] = gainers['Price Change'].astype(float)
+gainers.sort_values("Price Change", axis = 0, ascending = False, inplace = True)
+print (gainers) 
+
+'''
 sid = SentimentIntensityAnalyzer()
 
 NewsFeed = feedparser.parse("https://breakingthenews.net/news-feed.xml")
@@ -16,4 +36,4 @@ for x in range(0, len(NewsFeed.entries)):
     for k in sorted(ss):
         print('{0}: {1}, '.format(k, ss[k]), end='')
     print()
-
+'''
