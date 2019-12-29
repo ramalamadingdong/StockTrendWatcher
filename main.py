62de91526bec26e1500f50d7268b4f3225739531
rami <ramimouro@gmail.com>
2019-12-29 06:56:10 -0700

    cleaned up gainers, added average and then added live price

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
 import feedparser
+import json
 import googlefinance
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 import bs4 as bs
 import urllib.request
+from yahoo_fin import stock_info as si
+import datetime
+import time
 
-source = urllib.request.urlopen("https://finance.yahoo.com/gainers").read()
+gainers = si.get_day_gainers()
+gainers.columns = ['SYM', 'COMPANY', 'Current Price', 'Price Change', 'Percentage Change', 'Volume'	, 'Avg Vol', 'Market Cap', '52 Week Range']
+gainers.sort_values("Price Change", axis = 0, ascending = False, inplace = True)
+
+
+end = datetime.date.today()
+start = end - datetime.timedelta(days=200)
+SYM = "AAPL"
+end = str(int(time.mktime(end.timetuple())))
+start = str(int(time.mktime(start.timetuple())))
+
+url = 'https://finance.yahoo.com/quote/' + SYM +'/history?period1=' + start +'&period2=' + end + '&interval=1d&filter=history&frequency=1d'
+source = urllib.request.urlopen(url).read()
+
 soup = bs.BeautifulSoup(source, features="html.parser")
+
 table = soup.table
 table_rows = table.find_all('tr')
 rows = []
+
 for tr in table_rows:
     td = tr.find_all('td')
     row = [i.text for i in td]
     rows.append(row)
 rows.pop(0)
-gainers = pd.DataFrame(rows)
-gainers.columns = ['SYM', 'COMPANY', 'Current Price', 'Price Change', 'Percentage Change', 'Volume'	, 'Avg Vol', 'Market Cap', '52 Week Range', 'NaN']
-gainers['Price Change'] = gainers['Price Change'].str[1:]
-gainers['Price Change'] = gainers['Price Change'].astype(float)
-gainers.sort_values("Price Change", axis = 0, ascending = False, inplace = True)
-print (gainers) 
+rows.pop()
+rows.pop()
+
+s_hist_data = pd.DataFrame(rows)
+s_hist_data.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adjust Clsoe', 'Vol']
+
+s_hist_data['High'] = s_hist_data ['High'].astype(float)
+s_hist_data['Low']  = s_hist_data ['Low'].astype(float)
+
+#print(s_hist_data)
+#print(s_hist_data.mean(axis=0, skipna = True)) 
+var = s_hist_data.mean(axis=0, skipna = True)
+avg = (float(var[0]) + float(var[1]))/2
+
+print(si.get_live_price("aapl"))
+
+
+
+
 
 '''
 sid = SentimentIntensityAnalyzer()
